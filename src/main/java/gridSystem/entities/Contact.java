d2508ced8c4715aeb94d290923c0014dacb94a2f
asadali214 <14asadali@gmail.com>
2018-07-24 11:08:51 +0500

    test changes

--- src/main/java/gridSystem/entities/Contact.java
+++ src/main/java/gridSystem/entities/Contact.java
@@ -1,3 +1,4 @@
+
 package gridSystem.entities;
 
 import javax.persistence.Column;
@@ -16,7 +17,6 @@ public class Contact {
 	@Id @GeneratedValue
 	@Column(name = "id")
 	private int id;
-	
 	@Column(name = "first_name")
 	private String firstName;
 	@Column(name = "last_name")
